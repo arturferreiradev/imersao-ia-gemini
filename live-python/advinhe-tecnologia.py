7c38e147f4b0c27fc8193159b0aa756aa350fb65
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-08 20:28:22 -0300

    Jogo de Advinhação

---
+++ live-python/advinhe-tecnologia.py
@@ -0,0 +1,17 @@
+import requests;
+import random;
+
+# Variáveis criadas para armazenar os valores obtidos através da request
+url = 'https://raw.githubusercontent.com/guilhermeonrails/api-imersao-ia/main/words.json';
+resposta = requests.get(url);
+data = resposta.json();
+valor_secreto = random.choice(data);
+palavra_secreta = valor_secreto['palavra'];
+dica = valor_secreto['dica'];
+
+chute = input(f'A palavra secreta tem {len(palavra_secreta)} letras -> {dica}');
+
+if(chute == palavra_secreta):
+    print('Acertou');
+else:
+    print(f'Errou.. a palavra secreta era {palavra_secreta}');
